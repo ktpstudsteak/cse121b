c1c837e13839595be86a1ab01aa2b087ad3aab43
Kaden <kadenpayne@gmail.com>
2021-11-20 16:01:07 -0700

    week03 start

---
+++ task3.js
@@ -0,0 +1,10 @@
+function add(number1, number2){
+    return number1 + number2;
+}
+
+function addNumbers(){
+    let num1 = document.querySelector('#addend1').value;
+    let num2 = document.querySelector('#addend2').value;
+
+    
+}
